92a98807c87deaf582854263e3a95ee9f9da7340
Sophie Dunfield <dunfiena@gmail.com>
2024-04-01 14:26:55 -0300

    Sophie Commit
    Added addScore.php to project

--- php/addScore.php
+++ php/addScore.php
@@ -2,19 +2,22 @@
 require_once "crud.php";
 $crud = new crud;
 
-$json = json_decode($_REQUEST['data'], true);
+if (isset($_POST['data'])) {
+    $json = $_POST['data'];
+    $json = json_decode($json, true);
 
-if ($json === null){
-    print_r([], "invalid json");
-    die;
-}
+    if ($json === null) {
+        print_r([], "invalid json");
+        die;
+    } else {
+        $userID = $_SESSION["userID"];
+        $gameID = $json['gameID'];
+        $score = $json['score'];
 
-//$userID = $_SESSION["userID"];
-$gameID = $json['gameID'];
-$score = $json['score'];
-
-try {
-    $crud->addScore(1, $gameID, $score);
-} catch (Exception $e) {
-    die("err at delete account php: " . $e->getMessage());
+        try {
+            $crud->addScore($userID, $gameID, $score);
+        } catch (Exception $e) {
+            die("err at delete account php: " . $e->getMessage());
+        }
+    }
 }
